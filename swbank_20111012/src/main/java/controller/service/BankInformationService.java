07bfa5c95ba10142d26f22a20d2bd44ef62edde6
Lars-Christian Simon <lars-christian.simon@hotmail.de>
2011-10-19 17:25:52 +0200

    Removed PasswordServiceTest. Caused LinkageError "sometimes". BankInformationBean ready. Properties cannot be found. So, defaults are loaded. bankName = S & W Bank bankCode = 200 100 20

--- swbank_20111012/src/main/java/controller/service/BankInformationService.java
+++ swbank_20111012/src/main/java/controller/service/BankInformationService.java
@@ -1,5 +1,11 @@
 package controller.service;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Properties;
 
 import javax.annotation.PostConstruct;
@@ -13,6 +19,8 @@ import javax.inject.Named;
 import org.jboss.logging.Logger;
 import org.jboss.seam.solder.logging.Category;
 
+import util.BankConstants;
+
 @Singleton
 @ApplicationScoped
 @Startup
@@ -24,13 +32,31 @@ public class BankInformationService {
 	
 	private static BankInformationService instance;
 	
-	private String bankName = "No BankName";
-	private String bankCode = "No BankCode";
+	private String bankName = "S & W Bank";
+	private String bankCode = "200 100 20";
 	
+	@SuppressWarnings("unused")
 	@PostConstruct
 	private void loadBankInformation() {
-		log.info("loading bankinformations");
-//		Properties infs = 
+//		log.info("loading bankinformations");
+		Properties defaultProps = new Properties();
+		defaultProps.put(BankConstants.BANK_NAME_KEY, bankName);
+		defaultProps.put(BankConstants.BANK_CODE_KEY, bankCode);
+		Properties infs = new Properties(defaultProps);
+		try {
+			infs.load(new FileReader(new File(/*"C:/bank.properties")));*/new URI("file:///resources/bank.properties"))));
+		} catch (FileNotFoundException e) {
+			log.error(e);
+		} catch (IOException e) {
+			log.error(e);
+		} 
+		catch (URISyntaxException e) {
+			log.error(e);
+		}
+		this.bankName = infs.getProperty(BankConstants.BANK_NAME_KEY);
+		this.bankCode = infs.getProperty(BankConstants.BANK_CODE_KEY);
+		log.info("Bank: " + bankName);
+		log.info("BLZ: " + bankCode);
 	}
 
 	@Named
--- swbank_20111012/target/swbank_1.0.0/WEB-INF/classes/import.sql
+++
@@ -1,9 +0,0 @@
---insert into credentialbean (id, identity, pass) values (1, 'lcsn', '4711') 
---insert into credentialbean (id, identity, pass) values (2, 'sebi', '1703')
-
---insert into userbean (id, firstname, lastname, email, role, credentialid) values (1, 'lars', 'simon', 'lars-christian.simon@hotmail.de', 'DEVELOPER', 1)
---insert into userbean (id, firstname, lastname, email, role, credentialid) values (2, 'sebastian', 'wisniewski', 'sebastian_wisniewski@freenet.de', 'DEVELOPER', 2)
-
-
-
-
